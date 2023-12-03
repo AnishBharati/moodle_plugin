210cf1512967a07aff4a16ab7319e5eb799a4f21
RaghavSitaula <113698501+RaghavSitaula@users.noreply.github.com>
2023-12-03 12:38:41 +0545

    Courses and Grades table
    Two tables that shows courses that a student has taken and grades in each subjects.

--- control/manage.php
+++ control/manage.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->dirroot . '/local/control/classes/form/login.php');
 
-global $PAGE, $OUTPUT;
+global $PAGE, $OUTPUT, $USER, $DB;
 
 $PAGE->set_url(new moodle_url('/local/control/manage.php'));
 $PAGE->set_context(context_system::instance());
@@ -36,9 +36,70 @@ echo '<input type="submit" value="Logout">';
 echo '</form>';
 echo '</div>';
 
-echo $OUTPUT->render_from_template('local_control/manage', $templatecontext);
+// Sample data for grades with subjects
+$sampleDataGrades = array(
+    array('userid' => 1, 'grade' => 85, 'subject' => 'Math'),
+    array('userid' => 2, 'grade' => 92, 'subject' => 'English'),
+    // Add more sample data as needed
+);
 
-echo "Logged in as user ID: " . $USER->id;
+// Fetch data from the mdl_course and mdl_user tables
+$courseData = $DB->get_records_sql('
+    SELECT c.id AS courseid, c.fullname AS coursename, c.shortname, u.firstname
+    FROM {course} c
+    JOIN {enrol} e ON e.courseid = c.id
+    JOIN {user_enrolments} ue ON ue.enrolid = e.id
+    JOIN {user} u ON u.id = ue.userid
+');
+
+// Function to generate table HTML
+function generateTable($data, $tableName) {
+    $table = '<div style="width: 48%; margin: 1%; float: left;">';
+    $table .= '<div style="padding: 20px; border: 1px solid #ccc; border-radius: 5px;">';
+    $table .= '<h2 style="text-align: left;">' . $tableName . ' Information</h2>';
+    $table .= '<table style="width: 100%; border-collapse: collapse; margin-top: 15px;">';
+    $table .= '<thead style="background-color: #f2f2f2; border-bottom: 1px solid #ccc;">';
+
+    // Header row based on the table type
+    if ($tableName === 'Grades') {
+        $table .= '<tr><th style="padding: 10px; text-align: left;">User ID</th><th style="padding: 10px; text-align: left;">Grade</th><th style="padding: 10px; text-align: left;">Subject</th></tr>';
+    } elseif ($tableName === 'Courses') {
+        $table .= '<tr><th style="padding: 10px; text-align: left;">Course ID</th><th style="padding: 10px; text-align: left;">Course Name</th><th style="padding: 10px; text-align: left;">Short Name</th><th style="padding: 10px; text-align: left;">Student Name</th></tr>';
+    }
+
+    $table .= '</thead>';
+    $table .= '<tbody>';
+
+    // Data rows
+    foreach ($data as $entry) {
+        $table .= '<tr style="border-bottom: 1px solid #ccc;">';
+        foreach ($entry as $value) {
+            $table .= '<td style="padding: 10px;">' . $value . '</td>';
+        }
+        $table .= '</tr>';
+    }
+
+    $table .= '</tbody>';
+    $table .= '</table>';
+    $table .= '</div>';
+    $table .= '</div>';
+
+    return $table;
+}
+
+// Display the "Welcome" message in the center
+echo '<div style="text-align: center; margin: 20px;">';
+echo '<h1>Welcome</h1>';
+echo '<p style="text-align: center;">Logged in as user ID: ' . $USER->id . '</p>';
+echo '</div>';
+
+// Display the course table using data fetched from the database
+echo '<div style="overflow: auto;">'; // Parent container
+echo generateTable($sampleDataGrades, 'Grades');
+echo generateTable($courseData, 'Courses');
+echo '</div>';
+
+// Print the "Logged in as user ID" message below the tables
 
 echo $OUTPUT->footer();
 
